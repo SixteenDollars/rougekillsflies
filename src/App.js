786dba7b2640237f7d86b6897cbe26ecffa4e42c
Rouge <ry744@nyu.edu>
2019-08-21 02:09:09 -0400

    change

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import tidal from './resources/images/tidal.png'
 import pandora from './resources/images/pandora.png'
 import me from './resources/images/me.png'
 import ig from './resources/images/ig.png'
-import sc from './resources/images/sc.png'
+import song from './resources/songs/song.m4a'
 import 'bootstrap/dist/css/bootstrap.css';
+import ReactAudioPlayer from 'react-audio-player';
 
 class App extends Component {
 
@@ -25,8 +26,8 @@ class App extends Component {
 				<div class="container">
 					<div class="row">
 						<div class="col">
-							<p class="text-left" id="head"> HALF OF HEAVEN IS A HOME </p>	
-						</div>			
+							<p class="text-left" id="head"> HALF OF HEAVEN IS A HOME </p>
+						</div>
 					</div>
 					<div class="row justify-content-center align-items-center">
 						<div class="col-xs-12" id="title"><p class="text-center">STOP tweakin' (2018)</p></div>
@@ -53,17 +54,17 @@ class App extends Component {
 						</div>
 						<div class="col-sm-1 col-2">
 							<a href="https://geo.music.apple.com/us/album/_/1460176267?mt=1&app=music&at=1000lHKX">
-							<img class="img-fluid" id="apple" src={apple} alt=""></img>
+								<img class="img-fluid" id="apple" src={apple} alt=""></img>
 							</a>
 						</div>
 						<div class="col-sm-1 col-2">
 							<a href="https://open.spotify.com/album/6ITBoE1l1zPCtXKOTN12IC">
-							<img class="img-fluid" id="spotify" src={spotify} alt=""></img>
+								<img class="img-fluid" id="spotify" src={spotify} alt=""></img>
 							</a>
 						</div>
 						<div class="col-sm-1 col-2">
 							<a href="https://www.pandora.com/artist/various-artists/love-hart-presents-hartfm-explicit/AL2V7Pb7xcg7p74">
-							<img class="img-fluid" id="pandora" src={pandora} alt=""></img>
+								<img class="img-fluid" id="pandora" src={pandora} alt=""></img>
 							</a>
 						</div>
 					</div>
@@ -79,6 +80,22 @@ class App extends Component {
 							allowtransparency="true"
 						/>
 
+					</div>
+
+					<div class="row justify-content-center align-items-center">
+						<div class="col-xs-12" id="title3">
+							<p class="text-center">dollartree (rough demo) (w/ khaji, atlas, ryanwise) (2019) </p>
+						</div>
+					</div>
+
+					<div class="row justify-content-center align-items-center" id="soundcloud-player">
+						<ReactAudioPlayer
+							class="col-xs-12"
+							id = "play"
+							src={song}
+							autoPlay
+							controls
+						/>
 					</div>
 
 					<div class="row justify-content-center align-items-center">
