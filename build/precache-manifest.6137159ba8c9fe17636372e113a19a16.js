0390199386022420cdb10b1787769341bd181193
Rouge <ry744@nyu.edu>
2020-04-25 21:36:32 -0400

    Init

---
+++ build/precache-manifest.6137159ba8c9fe17636372e113a19a16.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53577e955355df1cb39503f5edb89eb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3a9d5e9842f350adeb7",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8b08005140aadce4848a",
+    "url": "/static/css/main.cdaab089.chunk.css"
+  },
+  {
+    "revision": "b3a9d5e9842f350adeb7",
+    "url": "/static/js/2.a322a812.chunk.js"
+  },
+  {
+    "revision": "8b08005140aadce4848a",
+    "url": "/static/js/main.850ea5e7.chunk.js"
+  },
+  {
+    "revision": "3ee15d4df232563c2d0b",
+    "url": "/static/js/runtime~main.70ccc2c3.js"
+  },
+  {
+    "revision": "4b1546b4bc5073ddb2c2e0e638fe4541",
+    "url": "/static/media/SoulMarkerHandwritingFont.4b1546b4.woff"
+  },
+  {
+    "revision": "b17f81bf8c2763b7848bfec5dc75e81e",
+    "url": "/static/media/SoulMarkerHandwritingFont.b17f81bf.woff2"
+  },
+  {
+    "revision": "3d919e8eeb9769073ad0661c033bb692",
+    "url": "/static/media/apple-music.3d919e8e.png"
+  },
+  {
+    "revision": "001d8490ebe1f677684a6518e0c0159f",
+    "url": "/static/media/deathwish (demo).001d8490.mp3"
+  },
+  {
+    "revision": "b5b8e847d713ecb4dd23f651206e217d",
+    "url": "/static/media/deathwish.b5b8e847.png"
+  },
+  {
+    "revision": "a8cc6acfd577e798aa59d1aa260ef226",
+    "url": "/static/media/ig.a8cc6acf.png"
+  },
+  {
+    "revision": "156acdcc268a15271ccd8608f6d68407",
+    "url": "/static/media/me.156acdcc.png"
+  },
+  {
+    "revision": "ee193b0b9f985468ea066a803b634742",
+    "url": "/static/media/pandora.ee193b0b.png"
+  },
+  {
+    "revision": "a9225b242e7b1678b9a947bfc950a48b",
+    "url": "/static/media/spotify.a9225b24.png"
+  },
+  {
+    "revision": "5baa6a444655f3b7a25548e0b006b481",
+    "url": "/static/media/tidal.5baa6a44.png"
+  }
+]);
