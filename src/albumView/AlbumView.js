83a51969b21b243b0bc6a4519dbe667883997d72
Rouge <ry744@nyu.edu>
2021-02-21 21:41:51 -0500

    Update

--- src/albumView/AlbumView.js
+++ src/albumView/AlbumView.js
@@ -1,52 +1,64 @@
 import React, { Component } from 'react'
 import './AlbumView.css';
+import ig from '../resources/images/ig.png'
 import 'bootstrap/dist/css/bootstrap.css';
-import appleblack from '../resources/images/appleblack.png'
-import bcblack from '../resources/images/bcblack.png'
-import spotifyblack from '../resources/images/spotifyblack.png'
-import pause from '../resources/images/pause.png'
-import play from '../resources/images/play.png'
-import tl from '../resources/images/TL.png'
-import tr from '../resources/images/TR.png'
-import bl from '../resources/images/BL.png'
-import br from '../resources/images/plant.png'
-import flower from '../resources/videos/08-08.m4v'
-import ReactPlayer from 'react-player'
-import CircleType from "circletype";
 import { Link } from 'react-router-dom'
+import war from '../resources/images/cover.png'
+import misc from '../resources/images/misc.png'
+import plant from '../resources/images/TR.png'
+import shoot1 from '../resources/images/shoot1.JPG'
+import ReactPlayer from 'react-player'
+import flower from '../resources/videos/08-08.m4v'
 
 class AlbumView extends Component {
-	constructor(props) {
-		super(props);
-	}
 
 	componentDidMount() {
-
+		// const circleType = new CircleType(document.getElementById("circleText")).radius(55);
 	}
 
 	render() {
 		return (
-			<div id="page">
-				<div class="container">
-					<div class="row">
-						<div class="col">
-							<Link to="/">
-								<p class="text-center" id="header"> HOME </p>
-							</Link>
-							<p class="text-center" id="header"> CONTACT </p>
-						</div>
-					</div>
-				</div>
-				<div id="header-2">
-
-				</div>
-				<div id="grid">
-
-				</div>
-				<div id="logos">
-
-				</div>
-			</div>
+			<main class="albumView">
+				<nav>
+					<a href="https://rougedied.com"><h1 id="header">HOME</h1></a>
+					<a href="https://rougedied.com/contact"><h1 id="header">CONTACT</h1></a>
+				</nav>				
+				<ul class="img_list">
+				<li>
+						<figure>
+						<ReactPlayer
+							class="col-xs-12"
+							url="https://www.youtube.com/watch?v=UTDQnOqUe-Q" 
+							controls
+						/>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<a href="https://www.instagram.com">
+								<img src={war} alt="WAR!"></img>
+							</a>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<a href="https://www.rougedied.com/misc">
+								<img src={misc} alt="Misc"></img>
+							</a>						
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<a href="https://www.instagram.com">
+								<img src={shoot1} alt="Shoot1"></img>
+							</a>
+						</figure>
+					</li>
+				</ul>
+				<a href="https://www.instagram.com/rougedied">
+					<img id="ig" class="mx-auto d-block" src={ig} alt=""></img>
+				</a>
+			</main>
 		);
 	}
 }
--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,20 +1,13 @@
-import ReactPlayer from 'react-player'
 import React, { Component } from 'react'
 import './Homepage.css';
-import apple from '../resources/images/apple-music.png'
-import bandcamp from '../resources/images/BandcampHoriz.png'
-import soundcloud from '../resources/images/SoundcloudReg.png'
-import spotify from '../resources/images/spotify.png'
-import tidal from '../resources/images/tidal.png'
-import pandora from '../resources/images/pandora.png'
 import ig from '../resources/images/ig.png'
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom'
 import war from '../resources/images/cover.png'
 import misc from '../resources/images/misc.png'
-import plant from '../resources/images/plant.png'
+import plant from '../resources/images/TR.png'
 import shoot from '../resources/images/shoot.jpeg'
-import CircleType from "circletype";
+import ReactPlayer from 'react-player'
 import flower from '../resources/videos/08-08.m4v'
 
 class Homepage extends Component {
@@ -25,78 +18,59 @@ class Homepage extends Component {
 
 	render() {
 		return (
-			<div id="homePage">
-				<div class="container">
-					<div class="row">
-						<div class="col">
-							<p class="text-left" id="head"> HALF OF HEAVEN IS A HOME </p>
-						</div>
-
-{/* 						<div class="col">
-							<div class="circular-text">
-								<div id="rotating">
-									<span id="circleText">
-										THE • WORLD • IS • YOURS • AND • EVERYTHING • IN • IT •
-								</span>
-								</div>
-							</div>
-						</div> */}
-					</div>
-
-					<div id="grid">
-						<div id="box on1">
-							<div id="box1x1">
-								<Link to="/war">
-									{<img class="img-fluid" src={war} alt=""></img>}
-								</Link>
-							</div>
-						</div>
-						<div id="box one2">
-							<div id="box1x2">
-								<Link to="/misc">
-									{<img class="img-fluid" src={misc} alt=""></img>}
-								</Link>
-							</div>
-						</div>
-						<div id="box one3">
-							<div id="box1x3">
-								<div id="video-container">
-									<ReactPlayer
-										id="video-player"
-										playing
-										url={flower}
-										width='100%'
-										height='100%'
-										muted={true}
-										loop
-									/>
-								</div>
-							</div>
-						</div>
-						<div id="box two1">
-							<div id="box2x1">
-								<Link to="/video">
-									{<img class="img-fluid" src={plant} alt=""></img>}
-								</Link>
-							</div>
-						</div>
-						<div id="box two2">
-							<div id="box2x2">
-								<Link to="/shoot">
-									{<img class="img-fluid" src={shoot} alt=""></img>}
-								</Link>
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-xs col-cent">
-							<a href="https://www.instagram.com/rougedied">
-								<img id="ig" class="mx-auto d-block" src={ig} alt=""></img>
+			<main class="homePage">
+				<nav>
+					<h1>HALF OF HEAVEN IS A HOME</h1>
+				</nav>	
+				<ul class="img_list">
+					<li>
+						<figure>
+							<a href="https://www.rougedied.com/war">
+								<img src={war} alt="WAR!"></img>
 							</a>
-						</div>
-					</div>
-				</div>
-			</div>
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<a href="https://www.rougedied.com/gallery">
+								<img src={shoot} alt="Shoot"></img>
+							</a>						
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<a href="https://www.rougedied.com/misc">
+								<img src={misc} alt="Misc"></img>
+							</a>						
+						</figure>
+					</li>
+					<li>
+						<figure>
+							<a href="https://www.rougedied.com/flower">
+								<img src={plant} alt="Flower"></img>
+							</a>						
+						</figure>
+					</li>
+{/* 					<li>
+						<figure>
+							<div id="video-container">
+								<ReactPlayer
+									id="video-player"
+									playing
+									url={flower}
+									width='100%'
+									height='100%'
+									muted={true}
+									loop
+								/>
+							</div>
+						</figure>
+					</li> */}
+				</ul>
+				<a href="https://www.instagram.com/rougedied">
+					<img id="ig" class="mx-auto d-block" src={ig} alt=""></img>
+				</a>
+			</main>
 		);
 	}
 }
