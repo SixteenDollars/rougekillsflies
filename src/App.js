51efc2ac63b9f2cfa591eb0f679d8d31a75e4f45
Rouge <ry744@nyu.edu>
2021-02-22 15:40:39 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import LandingPage from './landingPage/LandingPage'
+import Misc from './miscView/Misc'
 import Homepage from './homePage/Homepage'
 import Contact from './contact/Contact'
 import AlbumView from './albumView/AlbumView'
@@ -12,7 +12,7 @@ export default function App() {
 			<Switch>
 				<Route exact path="/" component={Homepage} />
 				<Route exact path="/war" component={AlbumView} />
-				<Route exact path="/misc" component={Homepage} />
+				<Route exact path="/misc" component={Misc} />
 				<Route exact path="/video" component={Homepage} />
 				<Route exact path="/shoot" component={Homepage} />
 				<Route exact path="/contact" component={Contact} />
--- src/albumView/AlbumView.js
+++ src/albumView/AlbumView.js
@@ -8,7 +8,10 @@ import misc from '../resources/images/misc.png'
 import plant from '../resources/images/TR.png'
 import shoot1 from '../resources/images/shoot1.JPG'
 import ReactPlayer from 'react-player'
-import flower from '../resources/videos/08-08.m4v'
+import spotifyblack from '../resources/images/spotifyblack.png'
+import appleblack from '../resources/images/appleblack.png'
+import tidalblack from '../resources/images/tidalblack.png'
+import bcblack from '../resources/images/bcblack.png'
 
 class AlbumView extends Component {
 
@@ -22,17 +25,28 @@ class AlbumView extends Component {
 				<nav>
 					<a href="https://rougedied.com"><h1 id="header">HOME</h1></a>
 					<a href="https://rougedied.com/contact"><h1 id="header">CONTACT</h1></a>
-				</nav>				
+				</nav>
+				<div class="youtube-player">
+					<ReactPlayer
+						url="https://www.youtube.com/watch?v=UTDQnOqUe-Q"
+						controls
+					/>
+				</div>
+				<div id="logos">
+					<div id="appleblack">
+						<img class="img-fluid" src={appleblack} alt=""></img>
+					</div>
+					<div id="spotifyblack">
+						<img class="img-fluid" src={spotifyblack} alt=""></img>
+					</div>
+					<div id="bcblack">
+						<img class="img-fluid" src={bcblack} alt=""></img>
+					</div>
+				</div>
+				<div class="album-text">
+					<h1 class="album-title">"WAR!" by Rouge</h1>
+				</div>
 				<ul class="img_list">
-				<li>
-						<figure>
-						<ReactPlayer
-							class="col-xs-12"
-							url="https://www.youtube.com/watch?v=UTDQnOqUe-Q" 
-							controls
-						/>
-						</figure>
-					</li>
 					<li>
 						<figure>
 							<a href="https://www.instagram.com">
@@ -44,7 +58,7 @@ class AlbumView extends Component {
 						<figure>
 							<a href="https://www.rougedied.com/misc">
 								<img src={misc} alt="Misc"></img>
-							</a>						
+							</a>
 						</figure>
 					</li>
 					<li>
