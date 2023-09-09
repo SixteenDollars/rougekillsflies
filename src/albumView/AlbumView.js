0ba5bf70fae463fde18021598ebcaffeaaf41208
Rouge <ry744@nyu.edu>
2023-09-09 00:14:36 -0400

    Release

--- src/albumView/AlbumView.js
+++ src/albumView/AlbumView.js
@@ -5,6 +5,7 @@ import ReactPlayer from 'react-player'
 import spotifyblack from '../resources/images/spotifyblack.png'
 import appleblack from '../resources/images/appleblack.png'
 import tidalblackcircle from '../resources/images/tidalblackcircle.png'
+import yt from '../resources/images/yt.png'
 import bcblack from '../resources/images/bcblack.png'
 import mask from '../resources/images/Frame 4.png'
 import linktree from '../resources/images/OTHER.png'
@@ -32,62 +33,32 @@ class AlbumView extends Component {
                     <div class="player-over-text-wrap">
                         <h1 class="player-over-text">"How To Make A Nail Bomb" (Single - 2023)</h1>
                     </div>
-                    <img id="nailbombart" class="mx-auto d-block" src={NailBombTease} alt=""></img>
-                    <div class="under-title">
-                        <h1 class="under-text">click below to pre-save on spotify</h1>
-                    </div>
-                    <div id="dsprow" class="row justify-content-center align-items-center">
-                        <div class="col-sm-1 col-md-1 col-2">
-                            <a href="https://distrokid.com/hyperfollow/rougekillsflies/how-to-make-a-nail-bomb">
-                                <img class="img-fluid" id="spotifywar" src={spotifyblack} alt=""></img>
-                            </a>
-                        </div>
-                    </div>
-                    </div>
-                </Animator>
-            </ScrollPage>
-{/*             <ScrollPage>
-                <Animator>
-                    <div id="section-2">
-                    <div class="player-over-text-wrap">
-                        <h1 class="player-over-text">"How To Make A Nail Bomb" (Single - 2023)</h1>
-                    </div>
                     <img id="nailbombart" class="mx-auto d-block" src={NailBombArt} alt=""></img>
                     <div class="under-title">
                         <h1 class="under-text">click on your streaming service</h1>
                     </div>
                     <div id="dsprow" class="row justify-content-center align-items-center">
                         <div class="col-sm-1 col-md-1 col-2">
-                            <a href="https://music.apple.com/us/album/war/1658193494">
+                            <a href="https://music.apple.com/us/album/how-to-make-a-nail-bomb/1704406232?i=1704406233">
                                 <img class="img-fluid" id="applewar" src={appleblack} alt=""></img>
                             </a>
                         </div>
                         <div class="col-sm-1 col-md-1 col-2">
-                            <a href="https://open.spotify.com/album/2IzE8WAuhm5AAj2xk4jKLe?si=CvlCDxjJRJOlxpoRCFX2OA">
+                            <a href="https://open.spotify.com/track/2jCN2k8Dx83AeB8TEx8Xys?si=2b8c9d03f7b5439a">
                                 <img class="img-fluid" id="spotifywar" src={spotifyblack} alt=""></img>
                             </a>
                         </div>
                         <div class="col-sm-1 col-md-1 col-2">
-                            <a href="https://listen.tidal.com/album/264649475">
+                            <a href="https://music.youtube.com/watch?v=dSMUrW9LqvI">
                                 <div class="img-max">
-                                    <img class="img-fluid" id="tidalwar" src={tidalblackcircle} alt=""></img>
+                                    <img class="img-fluid" id="yt" src={yt} alt=""></img>
                                 </div>
-                            </a>
-                        </div>
-                        <div class="col-sm-1 col-md-1 col-2">
-                            <a href="https://rougedied.bandcamp.com/album/war">
-                                <img class="img-fluid" id="bcwar" src={bcblack} alt=""></img>
-                            </a>
-                        </div>
-                        <div class="col-sm-1 col-md-1 col-2">
-                            <a href="https://album.link/i/1559758015">
-                                <img class="img-fluid" id="otherwar" src={linktree} alt=""></img>
                             </a>
                         </div>
                     </div>
                     </div>
                 </Animator>
-            </ScrollPage> */}
+            </ScrollPage>
             </main>
         </ScrollContainer>
 
