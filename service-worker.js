b6c2a9e0502901015551dbd9a9926da1669a9c0d
Rouge <ry744@nyu.edu>
2023-10-20 11:56:40 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a73934b6441ccaffa128ac4c2c56627.js"
+  "/precache-manifest.7d74fccea690ce0c9200b87596bd2c5c.js"
 );
 
 self.addEventListener('message', (event) => {
